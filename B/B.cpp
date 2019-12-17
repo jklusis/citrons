9dd5728ffd9e9fbeb22edf6fbf69a0125be77dc6
Jānis Kristaps Lūsis <35897850+jklusis@users.noreply.github.com>
2019-12-18 00:30:28 +0200

    Update and rename B1.cpp to B.cpp

--- B/B.cpp
+++ B/B.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int integerB1(int integer);
+int integerB3(int integer);
 int getIntegerLength(int integer);
 int getIntegerInTenthPower(int integer, int power);
 
@@ -23,6 +24,7 @@ int main ()
         
         cout << "Entered integer: " << integer << endl;
         cout << "Processed integer (B1): " << integerB1(integer) << endl;
+        cout << "Processed integer (B3): " << integerB3(integer) << endl;
 
         cout << "Continue?" << endl; 
         cin >> willContinue;
@@ -31,6 +33,7 @@ int main ()
     cout << "Exiting";
 }
 
+// Flip the last two numbers in integer
 int integerB1(int integer) 
 {
     int length = getIntegerLength(integer);
@@ -66,6 +69,52 @@ int integerB1(int integer)
 
         // The added number will be number * 10^(positionFromTheEnd - 1)
         processedInteger += getIntegerInTenthPower(currentNumber, endPosition - 1 + additionToPower);
+    }
+
+    return processedInteger;
+}
+
+// Remove 2nd and 3rd number from integer
+int integerB3(int integer) 
+{
+    int length = getIntegerLength(integer);
+
+    if (length == 0) {
+        return 0;
+    }
+
+    // If length is 1, simply return it.
+    if (length == 1) {
+        return integer;
+    }
+
+    // If length is 2, get rid of the last symbol
+    if (length == 2) {
+        return integer / 10;
+    }
+
+    // ........
+    if (length == 3) {
+        return integer / 10 / 10;
+    }
+
+    int processedInteger = 0;
+
+    for (int endPosition = 1; endPosition <= length; endPosition++) {
+        // Get the current number and get rid of it in the integer;
+        int currentNumber = integer % 10;
+        integer /= 10;
+
+        // If current number is the 3rd one, get rid of 2nd and 3rd numbers
+        if (length - (endPosition - 1) == 3) {
+            integer /= 10;
+
+            currentNumber = integer % 10;
+            processedInteger += getIntegerInTenthPower(currentNumber, endPosition - 1);
+            break;
+        }
+
+        processedInteger += getIntegerInTenthPower(currentNumber, endPosition - 1);
     }
 
     return processedInteger;
