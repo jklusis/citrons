13c519fbda5727a10a2e02fb73746c3d417f44ba
Jānis Kristaps Lūsis <janis.lusis@printful.com>
2020-05-03 16:05:29 +0300

    Init

---
+++ 2020/Lidostas/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Jānis Kristaps Lūsis
+ * JL17030
+ * 03.05.2020.
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Constant declarations
+
+const char* INPUT_FILE_NAME = "lidostas.in";
+const char* OUTPUT_FILE_NAME = "lidostas.out";
+
+int main()
+{
+    cout << "Hello there" << endl;
+
+    return 0;
+}
